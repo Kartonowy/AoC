480af745585c180ce5ca31fe18a9e33b7bb49328
Kartonowy <ttrollface.com.pl@gmail.com>
2024-06-26 14:33:12 +0200

    2023 day4

--- 2023/day4/src/main.rs
+++ 2023/day4/src/main.rs
@@ -1,27 +1,36 @@
-use std::fs::{read_to_string, File};
+use std::fs::{copy, read_to_string};
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Card<'a> {
     number: &'a str,
     winning_nums: String,
     card_nums: String,
+    copies: i32,
+}
+
+impl<'a> Card<'a> {
+    fn add_copy(&mut self) {
+        self.copies += 1;
+    }
 }
 
 fn main() {
     let input = read_to_string("input.txt").unwrap();
     let lines = input.lines().collect::<Vec<&str>>();
-    let cards: Vec<Card> = lines
+    let mut cards: Vec<Card> = lines
         .into_iter()
         .map(|line| {
             let mut split = line.split(['|', ':']);
             Card {
                 number: split.next().unwrap(),
-                winning_nums: split.next().unwrap().trim().replace("  ", " "), //.split(',').collect::<String>().split(' ').map(|x| -> i32{x.parse().expect("faild")}).collect(),
-                card_nums: split.next().unwrap().trim().replace("  ", " "), //.split(',').collect::<String>().split(' ').map(|x| -> i32{x.parse().unwrap()}).collect(),
+                winning_nums: split.next().unwrap().trim().replace("  ", " "),
+                card_nums: split.next().unwrap().trim().replace("  ", " "),
+                copies: 1,
             }
         })
         .collect();
-    let mut score = 0;
+    let mut copymap: [i32; 198] = [1; 198];
+    let mut index = 0;
     for card in cards {
         let winning: Vec<i32> = card
             .winning_nums
@@ -33,22 +42,27 @@ fn main() {
             .split(" ")
             .map(|n| n.parse::<i32>().unwrap())
             .collect();
+
         let mut cmatches = 0;
+
         winning.iter().for_each(|n| {
             if mine.contains(n) {
                 cmatches += 1;
             }
         });
 
-        score += match cmatches {
-            0 => 0,
-            1 => 1,
-            n @ _ => 2_i32.pow(n - 1),
-        };
+        // part 2 start
+        index += 1;
+        for n in index..=index + cmatches - 1 {
+            copymap[n] += 1;
+        }
+
         println!(
-            "{} {:?} \n {:?} matches: {} current score: {}",
-            card.number, winning, mine, cmatches, score
+            "{} {:?} \n {:?} matches: {}, {}",
+            card.number, winning, mine, cmatches, index
         );
     }
-    println!("{}", score)
+    let sum: i32 = copymap.iter().sum();
+    println!("{:?}", copymap);
+    println!("{:?}", sum)
 }
